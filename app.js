875325dceab483593714b183f02faf2c5eba5cc4
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2023-03-17 02:34:01 +0000

    JavaScript練習

---
+++ app.js
@@ -0,0 +1,45 @@
+// alert('Hello World');
+
+// 変数に代入
+// let hello = "Hello world";
+// alert(hello);
+
+// 演算
+// alert(4+3);
+
+// 文字の結合
+// alert("Hello" + "World");
+
+// 変数に代入しての結合
+// let str1 = "Hello";
+// let str2 = "World!!";
+// alert(str1+str2);
+
+// 条件分岐　if文
+// let orange = 100;
+// let apple = 200;
+// if(orange < apple){
+  // alert("みかんの値段がりんごよりも安い");
+// } else if(orange == apple){
+  // alert("みかんとりんごが同じ値段");
+// } else{
+  // alert("みかんの値段がりんごよりも高い");
+// }
+// 繰り返し構文　while文
+// let
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+  // num = num * 2;
+  // count = count + 1;
+// }
+
+// alert("2をかけ続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+// do~while文
+// do{
+  // 最低1回は行われる処理
+// }while(条件式);
+
