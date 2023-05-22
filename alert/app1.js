43a41d322a0ab7b7f8facd2b51b1221b7d1902aa
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2023-05-22 10:42:48 +0000

    JavaScript練習

---
+++ alert/app1.js
@@ -0,0 +1,16 @@
+alert('Hello World');
+
+// 変数に代入
+let hello = "Hello world";
+alert(hello);
+
+// 演算
+alert(4+3);
+
+// 文字の結合
+alert("Hello" + "World");
+
+// 変数に代入しての結合
+let str1 = "Hello";
+let str2 = "World!!";
+alert(str1+str2);
