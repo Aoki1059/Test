6520316b39980956e10118a6346fb2beff903757
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2023-06-02 09:57:27 +0000

    JavaScript練習

---
+++ alert/app-if.js
@@ -0,0 +1,10 @@
+// 条件分岐　if文
+let orange = 100;
+let apple = 200;
+if(orange < apple){
+  alert("みかんの値段がりんごよりも安い");
+} else if(orange == apple){
+  alert("みかんとりんごが同じ値段");
+} else{
+  alert("みかんの値段がりんごよりも高い");
+}
